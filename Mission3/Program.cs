2ee7b5a97677c33972fa30c3901b63f89d75ce12
Kate Opager <opagerk@byu.edu>
2023-01-23 09:05:16 -0700

    .

--- Mission3/Program.cs
+++ Mission3/Program.cs
@@ -5,8 +5,14 @@ namespace Mission3
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        { 
+            Console.WriteLine("Welcome to Tic-Tac-Toe!"); //Welcome the user to the program and explain the tic tac toe game
+            Console.WriteLine();
+            Console.WriteLine("Choose between one of the 9 spots on the board to place your mark.");
+            Console.WriteLine("Three marks in a row, whether diagonal, vertical, or horizontal, will result in a winner.");
+            char[] boardspots = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };  //The boardspots array will be used to store play marks on the 9 possible spots on the board
+            
+
         }
     }
 }
