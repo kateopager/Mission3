324564332504232119bcb016bebde972f37a9969
Kate Opager <opagerk@byu.edu>
2023-01-25 10:01:20 -0700

    .

---
+++ Mission3/Class1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Mission3
+{
+    public class Board
+    {
+        public Board()
+        {
+        }
+        //prints out the board
+        public void printBoard(char[] tttboard)
+        {
+            Console.WriteLine();
+            //second half of it is what will replace the numbers in the braces
+            Console.WriteLine("{ 0} | { 1} | { 2}", tttboard[0], tttboard[1], tttboard[2]);
+            Console.WriteLine("---------");
+            Console.WriteLine("{ 0} | { 1} | { 2}", tttboard[3], tttboard[4], tttboard[5]);
+            Console.WriteLine("---------");
+            Console.WriteLine("{ 0} | { 1} | { 2}", tttboard[6], tttboard[7], tttboard[8]);
+        }
+        public int updateBoard(char[] tttboard, int space, int player)
+        {
+            //checks if the space is already taken
+            if (tttboard[space - 1] != '0' && tttboard[space - 1] != 'X')
+            {
+                //since player is auto incremented, use mod to check which player is playing
+                if (player % 2 == 1)
+                {
+                    tttboard[space - 1] = 'X';
+                }
+                else
+                {
+                    tttboard[space - 1] = 'O';
+                }
+                return player;
+            }
+            //if taken, throw error
+            else
+            {
+                Console.WriteLine("This is space is already taken!");
+                return player--;
+            }
+        }
+        public bool gameOver(char[] tttboard)
+        {
+            // Horizontal Wins
+            if (tttboard[0] == tttboard[1] && tttboard[1] == tttboard[2])
+            {
+                Console.WriteLine("Player ” +tttboard[0] + ” wins!");
+                return true;
+            }
+            if (tttboard[3] == tttboard[4] && tttboard[4] == tttboard[5])
+            {
+                Console.WriteLine("Player ” +tttboard[3] + ” wins!");
+                return true;
+            }
+            if (tttboard[6] == tttboard[7] && tttboard[7] == tttboard[8])
+            {
+                Console.WriteLine("Player ” +tttboard[6] + ” wins!");
+                return true;
+            }
+            // Vertical Wins
+            if (tttboard[0] == tttboard[3] && tttboard[3] == tttboard[6])
+            {
+                Console.WriteLine("Player ” +tttboard[0] + ” wins!");
+                return true;
+            }
+            if (tttboard[1] == tttboard[4] && tttboard[4] == tttboard[7])
+            {
+                Console.WriteLine("Player ” +tttboard[1] + ” wins!");
+                return true;
+            }
+            if (tttboard[2] == tttboard[5] && tttboard[5] == tttboard[8])
+            {
+                Console.WriteLine("Player ” +tttboard[2] + ” wins!");
+                return true;
+            }
+            //Diagonal Wins
+            if (tttboard[0] == tttboard[4] && tttboard[4] == tttboard[8])
+            {
+                Console.WriteLine("Player ” +tttboard[0] + ” wins!");
+                return true;
+            }
+            if (tttboard[2] == tttboard[4] && tttboard[4] == tttboard[6])
+            {
+                Console.WriteLine("Player ” +tttboard[2] + ” wins!");
+                return true;
+            }
+            //Draw
+            if (tttboard[0] != '1' && tttboard[1] != '2' && tttboard[2] != '3' && tttboard[3] != '4' && tttboard[4] != '5' && tttboard[5] != '6' && tttboard[6] != '7' && tttboard[7] != '8' && tttboard[8] != '9')
+            {
+                Console.WriteLine("Game ended in a draw");
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
--- Mission3/Program.cs
+++ Mission3/Program.cs
@@ -6,41 +6,32 @@ namespace Mission3
         static void Main(string[] args)
         {
             bool playGame = true;
-            while (playGame)
+            while (playGame) //this first while loop begins the tic tac toe program. It will play unless boolean playGame is false
             {
                 int player = 1;
                 int turn = 1;
-                bool winner = false;
+                bool winner = false; //this declares the winner variable but unless true, then the game continues
                 Console.WriteLine("Welcome to Tic-Tac-Toe!"); //Welcome the user to the program and explain the tic tac toe game
                 Console.WriteLine();
                 Console.WriteLine("Choose between one of the 9 spots on the board to place your mark.");
                 Console.WriteLine("Three marks in a row, whether diagonal, vertical, or horizontal, will result in a winner.");
                 char[] boardspots = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };  //The boardspots array will be used to store play marks on the 9 possible spots on the board
-                // char[] UsedBoardSpots = new char[9]; //create an array that stores the USED board spots so they are not typed in twice
-                char mark = 'X';
-                // call board
+                char[] UsedBoardSpots = new char[9]; //create an array that stores the USED board spots so they are not typed in twice
+                Board b = new Board();
+                b.printBoard(boardspots); //Call the board method from the other class so that we can display the board visually
                 while (!winner)
                 {
-                    if (player == 1)
-                    {
-                        mark = 'X';
-                    }
-                    else
-                    {
-                        mark = 'O';
-                    }
                     bool validResponse = false;
                     char choice = '1';
                     while (!validResponse)
                     {
                         Console.WriteLine("\nPlayer " + player.ToString() + ", enter the number of the spot you would like to mark: "); //Prompt the player to select a spot for their turn
                         choice = Console.ReadKey().KeyChar;
-                        bool choiceFound = false;
+                        /*bool choiceFound = false;
                         for (int i = 0; i < boardspots.Length; i++)
                         {
                             if (boardspots[i] == choice)
                             {
-                                boardspots[i] = mark;
                                 choiceFound = true;
                                 break;
                             }
@@ -52,7 +43,7 @@ namespace Mission3
                         else
                         {
                             Console.WriteLine("\nInvalid Response. The number you entered has either been used or is not an option. Try again.");
-                        }
+                        }*/
                     }
                     /*for (int i = 0; i < boardspots.Length; i++)
                     {
@@ -62,8 +53,8 @@ namespace Mission3
                             break;
                         }
                     }*/
-                    // call board method
-                    bool playerWin = false; // call the other method and return true or false for if a player won
+                    int playerTurn = b.updateBoard(boardspots, choice, player);
+                    bool playerWin = b.gameOver(boardspots); // call the other method and return true or false for if a player won
                     if (playerWin)
                     {
                         winner = true;
@@ -71,14 +62,7 @@ namespace Mission3
                     }
                     else
                     {
-                        if (player == 1)
-                        {
-                            player++;
-                        }
-                        else if (player == 2)
-                        {
-                            player--;
-                        }
+                        player++;
                     }
                     if (turn == 9)
                     {
